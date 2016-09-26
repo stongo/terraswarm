3fda4ac0d002d4f1253305b1435ac03e77ddef85
Marcus Stong <stongo@gmail.com>
2016-09-26 10:07:11 -0400

    more hd space

--- dev-run.sh
+++ dev-run.sh
@@ -11,7 +11,7 @@ function cleanup {
 discovery=$(curl https://discovery.etcd.io/new?size=$replicas)
 cleanup
 while [ $i -lt $replicas ]; do
-	[ -e var_lib_swarm0${i}.img.qcow2 ] || qcow-tool create --size=10GiB var_lib_swarm0${i}.img.qcow2
+	[ -e var_lib_swarm0${i}.img.qcow2 ] || qcow-tool create --size=20GiB var_lib_swarm0${i}.img.qcow2
 	cp modules/swarm/templates/cloud-config.yaml tmp/user_data_0${i}
 	sed -i '' "s|\${name}|swarm-0${i}|g" ./tmp/user_data_0${i}
 	sed -i '' "s|\${discovery_token}|${discovery}|g" ./tmp/user_data_0${i}
